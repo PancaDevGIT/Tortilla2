e6562abb0133cfc2356cb6232ab1bffd195da794
PancaDev <87989063+PancaDevGIT@users.noreply.github.com>
2023-05-29 04:47:10 +0200

    Update

--- src/app.js
+++ src/app.js
@@ -1,43 +1,6 @@
 /**
  * @author PancaDev
  */
-const clientId = '1112566121984438352';
- const DiscordRPC = require('discord-rpc');
- const RPC = new DiscordRPC.Client({ transport: 'ipc'});
- 
- DiscordRPC.register(clientId);
-
- async function setActivity() {
-    if (!RPC) return;
-    RPC.setActivity({
-        details: `TortillaLand 2 | Publico`,
-        state: `Jugando...`,
-        startTimestamp: Date.now(),
-        largeImageKey: 'logo',
-        largeImageText: 'TortillaLand 2 - Publico',
-        instance: false,
-        buttons: [
-            {
-                label: '⭐ Discord Server',
-                url: 'https://discord.gg/uSutTYv6zB',
-            },
-
-            {
-                label: '🌐 Web',
-                url: 'https://dreamdark.net',
-            }
-        ]
-    });
-}
-
-RPC.on('ready', async () => {
-    setActivity();
-
-    setInterval(() => {
-        setActivity();
-    }, 86400 * 1000);
-});
-
 
 const { app, ipcMain } = require('electron');
 const { Microsoft } = require('minecraft-java-core');
